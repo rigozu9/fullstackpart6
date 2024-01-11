93ed2b6a4dd2db97799134b4c6d20ba5176fed47
Riku <134082872+rigozu9@users.noreply.github.com>
2024-01-11 15:13:38 +0200

    6.14 ja 6.15 done

--- redux-anecdotes/redux-anecdotes/src/components/AnecdoteForm.jsx
+++ redux-anecdotes/redux-anecdotes/src/components/AnecdoteForm.jsx
@@ -2,14 +2,17 @@ import { useDispatch } from 'react-redux'
 import { createAnecdote } from '../reducers/anecdoteReducer'
 import { createNotification } from '../reducers/notificationReducer'
 
+import anecdoteService from '../services/anecdotes'
+
 const NewAnecdote = () => {
   const dispatch = useDispatch()
 
-  const addAnecdote = (event) => {
+  const addAnecdote = async (event) => {
     event.preventDefault()
     const content = event.target.anecdote.value
     event.target.anecdote.value = ''
-    dispatch(createAnecdote(content))
+    const newanecdote = await anecdoteService.createNew(content)
+    dispatch(createAnecdote(newanecdote))
 
     dispatch(createNotification(`new anecdote created '${content}'`))
     setTimeout(() => {dispatch(createNotification(null))}, 5000)
--- redux-anecdotes/redux-anecdotes/src/components/AnecdoteList.jsx
+++ redux-anecdotes/redux-anecdotes/src/components/AnecdoteList.jsx
@@ -2,39 +2,42 @@ import { useSelector, useDispatch } from 'react-redux'
 import { voteAnecdote } from '../reducers/anecdoteReducer'
 import { createNotification } from '../reducers/notificationReducer'
 
-
-
 const Anecdotes = () => {
-    const anecdotes = useSelector(state => 
-      state.anecdotes
-        .filter(anecdote => anecdote.content.toLowerCase().includes(state.filter.toLowerCase()))
+  const anecdotes = useSelector((state) =>
+  state.anecdotes
+      .filter(
+        (anecdote) =>
+          typeof anecdote.content === 'string' &&
+          anecdote.content.toLowerCase().includes(state.filter.toLowerCase())
+      )
     )
-    const dispatch = useDispatch()
-  
-    const vote = (anecdote ) => {
-      dispatch(voteAnecdote(anecdote .id))
-      dispatch(createNotification(`Voted for '${anecdote.content}'`))
-      setTimeout(() => {dispatch(createNotification(null))}, 5000)
-    }
-    
-    const sortedAnecdotes = anecdotes.sort((a, b) => b.votes - a.votes)
+  console.log(anecdotes)
+  const dispatch = useDispatch()
 
-    return (
-      <div>
-        {sortedAnecdotes.map(anecdote =>
-          <div key={anecdote.id}>
-            <div>
-              {anecdote.content}
-            </div>
-            <div>
-              has {anecdote.votes}
-              <button onClick={() => vote(anecdote)}>vote</button>
-            </div>
-          </div>
-        )}
-      </div>
-    )
+  const vote = (anecdote ) => {
+    dispatch(voteAnecdote(anecdote .id))
+    dispatch(createNotification(`Voted for '${anecdote.content}'`))
+    setTimeout(() => {dispatch(createNotification(null))}, 5000)
   }
+  
+  const sortedAnecdotes = anecdotes.sort((a, b) => b.votes - a.votes)
+
+  return (
+    <div>
+      {sortedAnecdotes.map(anecdote =>
+        <div key={anecdote.id}>
+          <div>
+            {anecdote.content}
+          </div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => vote(anecdote)}>vote</button>
+          </div>
+        </div>
+      )}
+    </div>
+  )
+}
   
 
 
